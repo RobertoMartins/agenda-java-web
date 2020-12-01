8c789c319cae2f243165be709c9f6f9adf12dd7e
Roberto Murilo Martins Cordeiro <robertomartins@users.noreply.github.com>
2020-12-01 16:45:54 -0300

    Configurando a camada Controller no Servlet

---
+++ agenda-java-web/WebContent/agenda.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+<link rel="icon" href="images/favicon.ico">
+<link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+	<h1>Agenda de Contatos</h1>
+	<a href="" class="btn1">Novo contato</a>
+</body>
+</html>
---
+++ agenda-java-web/src/model/DAO.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DAO {
+	
+	/*MODULO DE CONEXAO*/
+	
+	//PARAMETROS DE CONEXAO
+	private String driver= "com.mysql.cj.jdbc.Driver";
+	private String url="jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user="root";
+	private String password = "murilo17";
+	
+	//METODOS DE CONEXAO
+	private Connection conectar() {
+		Connection con = null;
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	/*
+	public void testeConexao(){
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}*/
+
+}
