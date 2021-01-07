2afbed0d452c0708d60d22928c8e607c4165ecb1
Roberto Murilo Martins Cordeiro <robertomartins@users.noreply.github.com>
2021-01-07 20:43:26 -0300

    criando metodo insert

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #  Agenda de contatos  Java Web
-Projeto desenvolvido para estudo de Java na web. Consiste em uma agenda de contatos, utilizando alguns recursos como: servlets, jsp, DAO, MVC.
+
+### Em desenvolvimento...
+Projeto desenvolvido para estudo de Java na web. Consiste em uma agenda de contatos, utilizando alguns recursos como: servlets, jsp, DAO, MySql e MVC.
+
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,36 +1,57 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.DAO;
 
-@WebServlet(urlPatterns ={"/Controller","/main"})
+import model.DAO;
+import model.JavaBeans;
+
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
-       
-    public Controller() {
-        super(); 
-    }
+	JavaBeans contato = new JavaBeans();
+
+	public Controller() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String action = request.getServletPath();
 		System.out.println(action);
 		
-		if(action.equals("/main"));
+		if(action.equals("/main")) {
 		contatos(request,response);
+		}else if(action.equals("/insert")) {
+			novoContato(request,response);
+				
+			}else {
+				response.sendRedirect("index.html");
+		}
 		
 	}
-	
-	//Listar contatos
-	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+	// Listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 	}
 	
+	// novo contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
+	}
 
 }
